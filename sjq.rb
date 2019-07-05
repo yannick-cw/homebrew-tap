fe4ca359a68f66a1c334e8273d9ee122e3de4be6
Yannick Gladow <yannick-cw@users.noreply.github.com>
2019-07-05 11:37:03 +0200

    Update version

--- sjq.rb
+++ sjq.rb
@@ -3,7 +3,7 @@ class Sjq < Formula
   homepage "https://github.com/yannick-cw/sjq"
   url "https://github.com/yannick-cw/sjq/releases/download/0.0.2/sjq-0.0.2.tar.gz"
   sha256 "55a6105882db1f0dc9c95ee16f9c7954ff9c79a8f3557f729dedff3b28cc1755"
-  version "0.0.1"
+  version "0.0.2"
   
   def install
     prefix.install "lib"
