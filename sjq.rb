bae06d7c54919bbb7d949a2aeaf7a8b894979f7d
Yannick Gladow <yannick-cw@users.noreply.github.com>
2019-07-22 11:30:04 +0200

    Update sjq.rb

--- sjq.rb
+++ sjq.rb
@@ -1,9 +1,9 @@
 class Sjq < Formula
   desc "Command-line JSON processor with Scala syntax"
   homepage "https://github.com/yannick-cw/sjq"
-  url "https://github.com/yannick-cw/sjq/releases/download/v0.0.3/sjq-0.0.3.tar.gz"
-  sha256 "25ddfc6ef3eef12b31229dbd07fcb8ac218fd24ae7eedef54f2e0e770d4ce222"
-  version "0.0.3"
+  version "0.0.4"
+  url "https://github.com/yannick-cw/sjq/releases/download/0.0.4/sjq-0.0.4.tar.gz"
+  sha256 "202f63e44a90f3ec69b946663ab5a8e488026dc60060f53ff5395d678107d55b"
   
   def install
     prefix.install "lib"
