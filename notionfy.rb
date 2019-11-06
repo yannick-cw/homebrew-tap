a149b005bf5d4257d4eb8eace935ccaf4e29cb02
Yannick Gladow <yannick-cw@users.noreply.github.com>
2019-11-06 09:45:38 +0100

    Update notionfy.rb

--- notionfy.rb
+++ notionfy.rb
@@ -1,7 +1,7 @@
 class Notionfy < Formula
   desc "Syncing kindle highlights to notion"
   homepage "https://github.com/yannick-cw/notionfy"
-  url "https://github.com/yannick-cw/notionfy/releases/download/0.1/notionfy-exe-0.1.tar.gz"
+  url "https://github.com/yannick-cw/notionfy/releases/download/0.1/notionfy-0.1.tar.gz"
   version "0.1"
   sha256 "fb3f0ad1d303ac6ab03395d173ce6eeb9a94520be923545714c3ba0c90da85f5"
   
