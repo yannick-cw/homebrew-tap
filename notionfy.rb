750656a1f2f6a7f829455f90c0dfac03919784e5
Yannick Gladow <yannick-cw@users.noreply.github.com>
2019-11-06 09:48:13 +0100

    Update notionfy.rb

--- notionfy.rb
+++ notionfy.rb
@@ -3,7 +3,7 @@ class Notionfy < Formula
   homepage "https://github.com/yannick-cw/notionfy"
   url "https://github.com/yannick-cw/notionfy/releases/download/0.1/notionfy-exe-0.1.tar.gz"
   version "0.1"
-  sha256 "fb3f0ad1d303ac6ab03395d173ce6eeb9a94520be923545714c3ba0c90da85f5"
+  sha256 "0f9d52281dab282a312306ba410df948abbefde111518ab949cf084ce646ffca"
   
   def install
     prefix.install "lib"
