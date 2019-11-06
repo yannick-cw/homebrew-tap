59b6c21f5b2cef5cbead4fd2b43c2d292ff520ff
Yannick Gladow <yannick-cw@users.noreply.github.com>
2019-11-06 09:33:54 +0100

    Update notionfy.rb

--- notionfy.rb
+++ notionfy.rb
@@ -1,13 +1,13 @@
 class Notionfy < Formula
-  desc "Sync your kindle highlights to Notion"
+  desc "Syncing kindle highlights to notion"
   homepage "https://github.com/yannick-cw/notionfy"
-  version "0.0.1"
-  url "https://github.com/yannick-cw/notionfy/releases/download/0.1/notionfy-0.0.1.tar.gz"
-  sha256 "6dc31b359b53193d14e731937499272386c5b9c05f35a68d0088af181df61fa3"
-
+  url "https://github.com/yannick-cw/notionfy/releases/download/0.1/notionfy-0.1.tar.gz"
+  sha256 "fb3f0ad1d303ac6ab03395d173ce6eeb9a94520be923545714c3ba0c90da85f5"
+  
   def install
-    prefix.install "lib"
-    bin.install "bin/notionfy-exe"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
   end
-
 end
