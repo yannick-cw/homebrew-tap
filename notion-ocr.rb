5d90cf2631346ba76e87ee0e3832c458e70aa114
Yannick Gladow <yannick-cw@users.noreply.github.com>
2019-11-16 16:42:14 +0100

    Update notion-ocr.rb

--- notion-ocr.rb
+++ notion-ocr.rb
@@ -3,7 +3,7 @@ class NotionOcr < Formula
   homepage "https://github.com/yannick-cw/notion-ocr"
   version "0.1"
   url "https://github.com/yannick-cw/notion-ocr/releases/download/v0.1/notion-ocr-0.1.tar.gz"
-  sha256 "c3f58c66bbeb824f91c4259fbda681b99cd2f3072a5c2e8b25720912ce5daaf9"
+  sha256 "8dc6a309084081d72d9791362f3429e865972a098f0709ac9151f9b95eeef5b0"
   
   def install
     bin.install "bin/notion-ocr"
