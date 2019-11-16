9f48daae246df0422c72d1bd48061d127ec79060
Yannick Gladow <yannick-cw@users.noreply.github.com>
2019-11-16 16:17:54 +0100

    Update notion-ocr.rb

--- notion-ocr.rb
+++ notion-ocr.rb
@@ -2,8 +2,8 @@ class Notion-Ocr < Formula
   desc "Adding ocr to images in notion"
   homepage "https://github.com/yannick-cw/notion-ocr"
   version "0.1"
-  url "https://github.com/yannick-cw/notion-ocr/releases/download/0.1/notion-ocr-0.1.tar.gz"
-  sha256 "295033bc7800135ac47866e4752be6de07195e7afe59dbbf9335d8c31812327c"
+  url "https://github.com/yannick-cw/notion-ocr/releases/download/v0.1/notion-ocr-0.1.tar.gz"
+  sha256 "c3f58c66bbeb824f91c4259fbda681b99cd2f3072a5c2e8b25720912ce5daaf9"
   
   def install
     bin.install "bin/notion-ocr"
