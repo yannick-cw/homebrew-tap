d78bd0e3f04e727622c22195a08a10f33fb36e3a
Yannick Gladow <yannick.gladow@googlemail.com>
2020-09-06 17:01:58 +0200

    Update notionfy.rb

--- notionfy.rb
+++ notionfy.rb
@@ -3,7 +3,7 @@ class Notionfy < Formula
   homepage "https://github.com/yannick-cw/notionfy"
   version "0.2.4"
   url "https://github.com/yannick-cw/notionfy/releases/download/0.2.4/notionfy_osx.tar.gz"
-  sha256 "2b9c76bd9ff8cdcff7efaecefe3a76e81726878c05282fe70b4151514e42bad5"
+  sha256 "8474c140b519b996cae45899818a9f3328a13a4d20de114a5470386cace7e883"
   
   def install
     bin.install "notionfy"
